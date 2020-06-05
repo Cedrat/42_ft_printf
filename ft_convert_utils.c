027e9a5fe83a397caf8a4f5624849998c8c95ba6
Cedrat <loicnoai@gmail.com>
2020-06-06 00:52:42 +0200

    fix null c?

--- ft_convert_utils.c
+++ ft_convert_utils.c
@@ -6,7 +6,7 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/31 18:20:47 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/06/06 00:47:06 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/06/06 00:51:28 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ int	ft_put_d_i(t_list *list, int nb)
 int	ft_put_c(t_list *list, char c, int i)
 {
 	if (c == 0)
-		return (ft_putnull(list));
+		return (ft_put_c(list, '\x00', 'c'));
 	if ((list->indic == '0' && i == 'c') || list->indic == -1)
 		ft_write_x(' ', list->width - 1);
 	else if (list->indic == '0' && i == '%')
